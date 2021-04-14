05899ce1f1bb42dda65e54260b7c5c7601583311
goosescout <michael.gurevich.02@mail.ru>
2021-04-14 09:15:08 +0700

    Sender update

--- ESP32_sender/ESP32_sender.ino
+++ ESP32_sender/ESP32_sender.ino
@@ -1,20 +1,72 @@
-#include <IRremoteESP8266.h>
-#include <IRsend.h>
+#include "IRremoteESP8266.h"
+#include "IRsend.h"
+
+#include "WiFi.h"
+#include "AsyncTCP.h"
+#include "WiFiClient.h"
+#include "ESPAsyncWebServer.h"
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+#define SERVER_NAME "ESP32_infared_server"
+#define SERVER_PASSWORD "2A(2v_!!*qaL"
 
 #define sender_pin 27  // any empty pin
 
+AsyncWebServer server(80);
+
 IRsend irsend(sender_pin);
 
-uint16_t raw_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+bool player_started = false;
+
+void processRequest(AsyncWebServerRequest *request) {
+    AsyncWebParameter* param = request->getParam(0);
+    if (param->value() == "1") {
+        if (player_started) {
+            myDFPlayer.start();
+        } else {
+            myDFPlayer.loop(1);
+            player_started = true;
+        }
+    } else {
+        myDFPlayer.pause();
+    }
+    Serial.print("param value recieved: ");
+    Serial.println(param->value());
+
+    request->send(200, "text/plain", "success");
+}
 
 void setup(void) {
     Serial.begin(115200);
     Serial.println("Starting ESP32 and Infrared...");
     irsend.begin();
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(SERVER_NAME, SERVER_PASSWORD);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    server.on("/", HTTP_GET, processRequest);
+    server.begin();
+
+    myDFPlayer.begin(mySoftwareSerial);
+    Serial.println("DFPlayer Mini online");
+    myDFPlayer.setTimeOut(100);  // timeout - 100ms
+    myDFPlayer.volume(10);  // set volume value (0-30).
 }
 
 void loop(void) {
-    Serial.println("Sending RAW data...");
-    irsend.sendRaw(raw_data, 10, 38);
-    delay(2000);
+    static unsigned long timer = millis();
+    if (millis() - timer > 500) {
+        timer = millis();
+        irsend.sendNEC(0x000105FFUL);  // data - 0105FF
+    }
 }
--- ESP8266_reciever/ESP8266_reciever.ino
+++ ESP8266_reciever/ESP8266_reciever.ino
@@ -12,13 +12,38 @@ void setup(void) {
     Serial.begin(115200);
     Serial.println("Starting ESP8266 and Infrared...");
     irrecv.enableIRIn();
+    irrecv.blink13(true);
 }
 
 void loop(void) {
     if (irrecv.decode(&results)) {
+        dump(&results);
         serialPrintUint64(results.value, HEX);
         Serial.println("");
         irrecv.resume();
     }
     delay(100);
+}
+
+void dump(decode_results *results) {
+    int count = results->rawlen;
+    if (results->decode_type == UNKNOWN) {
+        Serial.print("Unknown encoding: ");
+    }
+    else if (results->decode_type == NEC) {
+        Serial.print("Decoded NEC: ");
+    }
+    else if (results->decode_type == SONY) {
+        Serial.print("Decoded SONY: ");
+    }
+    else if (results->decode_type == RC5) {
+        Serial.print("Decoded RC5: ");
+    }
+    else if (results->decode_type == RC6) {
+        Serial.print("Decoded RC6: ");
+    } else {
+        Serial.print("NOOO... MY DECODING COLLECTION ");
+        Serial.print(results->decode_type);
+        Serial.print(" ");
+    }
 }
